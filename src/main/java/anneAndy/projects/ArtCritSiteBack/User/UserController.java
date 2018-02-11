df8675012ccb079b5fcab3ee15f480b805c5a205
Julia <jkmcgeoghan@myseneca.ca>
2018-02-10 22:38:12 -0500

    Add findbyuser request mapping and repository query

--- src/main/java/anneAndy/projects/ArtCritSiteBack/User/UserController.java
+++ src/main/java/anneAndy/projects/ArtCritSiteBack/User/UserController.java
@@ -3,7 +3,9 @@ package anneAndy.projects.ArtCritSiteBack.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,6 +22,12 @@ public class UserController {
 	public @ResponseBody Iterable<User> getAllUsers() {
 		return userRepository.findAll();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ResponseBody
+    public User findOne(@PathVariable("id") int id) {
+        return userRepository.findByIdUser(id);
+    }
 	
 }
 
--- src/main/java/anneAndy/projects/ArtCritSiteBack/User/UserRepository.java
+++ src/main/java/anneAndy/projects/ArtCritSiteBack/User/UserRepository.java
@@ -3,5 +3,5 @@ package anneAndy.projects.ArtCritSiteBack.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	User findByIdUser (int id);
 }
