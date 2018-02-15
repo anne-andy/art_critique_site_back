cd321a759eb6c20c25312590b6d3d9cc791d01fe
Julia <jkmcgeoghan@myseneca.ca>
2018-02-14 23:15:48 -0500

    Add content and image entities

---
+++ src/main/java/anneAndy/projects/ArtCritSiteBack/Image/Image.java
@@ -0,0 +1,37 @@
+//package anneAndy.projects.ArtCritSiteBack.Image;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//import Content.Content;
+//
+//@Entity
+//@Table(name = "Image")
+//
+//public class Image {
+//	
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.AUTO)
+//	private Integer idContent;
+//	private String file;
+//	private Content content;
+//	
+//	public Integer getContentId() {
+//		return idContent;
+//	}
+//	
+//	public String getFile() {
+//		return file;
+//	}
+//	public void setFile(String file) {
+//		this.file = file;
+//	}
+//	
+//	public void setContent(Content content) {
+//		this.content = content;
+//	}
+//
+//}
--- src/main/java/anneAndy/projects/ArtCritSiteBack/User/UserController.java
+++ src/main/java/anneAndy/projects/ArtCritSiteBack/User/UserController.java
@@ -24,53 +24,10 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	@ResponseBody
-    public User findOne(@PathVariable("id") int id) {
+    public @ResponseBody User findOne(@PathVariable("id") int id) {
         return userRepository.findByIdUser(id);
     }
 	
 }
 
-
-
-
-
-//package SpringBootTutorial.SpringBootTutorial;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import SpringBootTutorial.SpringBootTutorial.Greeting;
-//import SpringBootTutorial.SpringBootTutorial.GreetingRepository;
-//
-//@Controller  
-//@RequestMapping(path="/greeting")
-//public class GreetingController {
-//	
-//	@Autowired
-//	private GreetingRepository greetingRepository;
-//
-//    @GetMapping(path="/add")
-//    public @ResponseBody String addGreeting(@RequestParam String content) {
-//    	
-//    	Greeting greeting = new Greeting();
-//    	greeting.setContent(content);
-//    	//greeting.setId(1);
-//    	greetingRepository.save(greeting);
-//    	System.out.println(greeting);
-//    	return "Saved new greeting!";
-//    	
-//    }
-//    
-//    @GetMapping(path="/all")
-//	public @ResponseBody Iterable<Greeting> getAllUsers() {
-//		// This returns a JSON or XML with the users
-//		return greetingRepository.findAll();
-//	}
-//    
-//}
 
